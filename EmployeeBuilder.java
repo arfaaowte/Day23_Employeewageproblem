d181ca7cef4576607d9ab227690bf35fa2f43fa5
aowtearfa@gmail.com <aowtearfa@gmail.com>
2020-08-26 20:36:09 +0530

    Check attendance

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -1,6 +1,32 @@
-public class EmployeeBuilder{
+import java.util.*;
+
+class attendance
+{
+	public void attendancecheck(){
+		Random rand = new Random();
+
+                // Generate random integers in range 0 to 1
+        	int rand_int1 = rand.nextInt(2);
+        
+	       	// Check Attendance
+		if(rand_int1 == 0)
+		{
+        		System.out.println("Employee is Absent");
+		}
+		else
+		{
+			System.out.println("Employee is Present");
+		}
+	}
+}
+
+
+public class EmployeeBuilder
+{
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Employee wage computation program");
+	        attendance atd = new attendance();
+		atd.attendancecheck();
 	}
 }
