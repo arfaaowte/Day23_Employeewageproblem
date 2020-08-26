bf103c5f31e8583d01bb8d55b21103fd11cfd3a5
aowtearfa@gmail.com <aowtearfa@gmail.com>
2020-08-26 21:12:23 +0530

    Compute employee wage for full day

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -1,8 +1,11 @@
 import java.util.*;
 
+//class to check attendance
 class attendance
 {
 	public void attendancecheck(){
+		
+		//create instance of random class
 		Random rand = new Random();
 
                 // Generate random integers in range 0 to 1
@@ -20,7 +23,19 @@ class attendance
 	}
 }
 
+//class to computewage of employee
+class computewage
+{
+	public void wagecalculation()
+	{
+		final int EMP_RATE_PER_HOUR = 20;
+		final int HOURS_FULL_DAY = 8;
+		int totalwage = EMP_RATE_PER_HOUR*HOURS_FULL_DAY;
+		System.out.println("Employee wage per day is: "+totalwage);
+	}
+}
 
+//main class
 public class EmployeeBuilder
 {
 	public static void main(String args[])
@@ -28,5 +43,8 @@ public class EmployeeBuilder
 		System.out.println("Welcome to Employee wage computation program");
 	        attendance atd = new attendance();
 		atd.attendancecheck();
+		computewage cmpt = new computewage();
+		cmpt.wagecalculation();
+
 	}
 }
