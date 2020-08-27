243d99f4b26fdc4bea405b50c8048348ea2aac77
aowtearfa@gmail.com <aowtearfa@gmail.com>
2020-08-27 19:20:15 +0530

    Part time employee wage calculation

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -8,9 +8,17 @@ class computewage
 	{
 		final int EMP_RATE_PER_HOUR = 20;
 		final int HOURS_FULL_DAY = 8;
-		int totalwage = EMP_RATE_PER_HOUR*HOURS_FULL_DAY;
-		System.out.println("Employee wage per day is: "+totalwage);
+		//int totalwage = EMP_RATE_PER_HOUR*HOURS_FULL_DAY;
+		//System.out.println("Full time Employee wage per day is: "+totalwage);
 	}
+	public void part_time_wagecalculation()
+        {
+                final int EMP_RATE_PER_HOUR = 20;
+                final int HOURS_FULL_DAY = 4;
+                int totalwage = EMP_RATE_PER_HOUR*HOURS_FULL_DAY;
+                System.out.println("Part time Employee wage per day is: "+totalwage);
+        }
+
 }
 
 //main class
@@ -21,6 +29,7 @@ public class EmployeeBuilder
 		System.out.println("Welcome to Employee wage computation program");
 		computewage cmpt = new computewage();
 		cmpt.wagecalculation();
+		cmpt.part_time_wagecalculation();
 
 	}
 }
