ab0f2e32e01766334c18d06fec2cc422cb90c249
aowtearfa@gmail.com <aowtearfa@gmail.com>
2020-08-27 18:50:40 +0530

    Removed the display message

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -25,7 +25,6 @@ public class EmployeeBuilder
 {
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to Employee wage computation program");
 	        attendance atd = new attendance();
 		atd.attendancecheck();
 	}
