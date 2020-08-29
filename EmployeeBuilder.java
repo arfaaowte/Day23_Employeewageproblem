9698312440bef56763c750ab74e46252a6b652e4
aowtearfa@gmail.com <aowtearfa@gmail.com>
2020-08-29 10:34:53 +0530

    Computing wage for 2 companies

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -18,12 +18,47 @@ import java.util.*;
 //class to computewage of employee
 class computewage
 {
+	//company 1 wage calculation
+	public void wagecalculation_company1()
+        {
+                final int EMP_RATE_PER_HOUR = 40;
+                final int HOURS_FULL_DAY = 10;
+                final int IS_PART_TIME = 1;
+                final int IS_FULL_TIME = 0;
+                final int NUM_OF_WORKING_DAYS = 25;
+                final int MAX_HRS_IN_MONTH = 250;
+                int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+                //computation
+                while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+                {
+                        int empCheck = (int) (Math.floor(Math.random() * 10) %2);
+                        totalWorkingDays++;
+                        //switchcase to check fulltime or parttime
+                        switch (empCheck) {
+                                case IS_FULL_TIME:
+                                        empHrs = 10;
+                                        break;
+                                case IS_PART_TIME:
+                                        empHrs = 5;
+                                        break;
+                                default:
+                                        empHrs = 0;
+                                        break;
+                        }
+                        totalEmpHrs += empHrs;
+                        System.out.println("Days : "+totalWorkingDays + "Emp per Hr : "+empHrs);
+                }
+                int totalEmpWage = empHrs*EMP_RATE_PER_HOUR*NUM_OF_WORKING_DAYS;
+                System.out.println("Total Employee Monthly wage is :" + totalEmpWage);
+                }
+
 	public void wagecalculation()
 	{	
 		final int EMP_RATE_PER_HOUR = 20;
                 final int HOURS_FULL_DAY = 8;
                 final int IS_PART_TIME = 1;
-                final int IS_FULL_TIME = 2;
+                final int IS_FULL_TIME = 0;
 		final int NUM_OF_WORKING_DAYS = 20;
 		final int MAX_HRS_IN_MONTH = 200;
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
@@ -31,7 +66,7 @@ class computewage
 		//computation
 		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 		{
-			int empCheck = (int) (Math.floor(Math.random() * 10) %3);
+			int empCheck = (int) (Math.floor(Math.random() * 10) %2);
 			totalWorkingDays++;
 			//switchcase to check fulltime or parttime
 			switch (empCheck) {
@@ -61,6 +96,6 @@ public class EmployeeBuilder
 		System.out.println("Welcome to Employee wage computation program");
 		computewage cmpt = new computewage();
 		cmpt.wagecalculation();
-
+		cmpt.wagecalculation_company1();
 	}
 }
